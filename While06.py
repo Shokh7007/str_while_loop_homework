8bac0f69c92af76353c19ef3453ddaa597ffba48
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-04-28 15:02:25 +0500

    6

--- While06.py
+++ While06.py
@@ -1,3 +1,4 @@
+import string
 def main(s):
     """
     A variable of type str is given. Find and return how many consonant letters there are.
@@ -7,3 +8,11 @@ def main(s):
     Returns:
         int: return answer
     """
+    a=0
+    k=0
+    while a<len(s):
+        if s[a]=='o' or s[a]=='u' or s[a]=='a' or s[a]=='e' or s[a]=='i':
+            k+=1
+        a+=1
+    return k
+print(main('7uh6ygt5ftujj87fhef7igj'))
